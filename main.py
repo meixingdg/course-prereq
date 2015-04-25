f14c41a96ae0e66c67dfe9fe4ccf8192b4a96159
jay <jay.w.jensen@gmail.com>
2015-04-25 16:52:56 -0400

    Distinct background colors for ORCLAUSEs

--- main.py
+++ main.py
@@ -8,6 +8,17 @@ from models import Course
 from google.appengine.ext import ndb
 from prereqStringParser import *
 
+colors = ["#FE642E","#8181F7","#DA81F5","#D0FA58","#58FAF4","#FA5858","#FE2EC8"] #temporary hardcoded list of colors - easily expandable
+
+class CourseNode(): #just a simple class for use with jinja that keeps track of needed attributes for rendering the tree
+    def __init__(self,baseCourse, depth = 0, color = "#FF0000"):
+        self.backgroundColor = color
+        self.depth = depth
+        self.name = baseCourse.name
+        self.departmentCode = baseCourse.departmentCode
+        self.code = baseCourse.code
+        
+
 #Temporarily hardcoded a list of department codes.
 DEPARTMENTCODES = ["ADMN", "ARCH", "ARTS", "ASTR",
         "BCBP", "BIOL", "BMED", "CHEM",
@@ -30,18 +41,24 @@ def getStuff(dep, code):
     entry = ndb.gql("SELECT * FROM Course WHERE departmentCode = '" + dep + "' and code = " + code)
     return entry.fetch(1)
 
-def buildPrereqTree(dep, code, tree, depth=0):
+def buildPrereqTree(dep, code, tree, depth=0, color = 0):
     course = getStuff(dep, code)
-    for i in course:
-        tree.append((depth, i.name, i.code, i.departmentCode))
-        prereqs = getCourses(i.prereqs)
-        for preorco in prereqs:
-            for orclause in preorco:
-                for singleCourse in orclause:
-                    listCourse = splitCourseString(singleCourse)
+    for i in course: #getStuff returns a singleton list of courses
+        node = CourseNode(i, depth, colors[color%len(colors)]) #builds a node out of what we got from getStuff,
+                                                               #with depth of whatever we're at and the next color, looping if needed
+        prereqs = getCourses(i.prereqs) #parses the prereqs string into the form [PREREQS, COREQS] where PREREQS 
+                                        #is a list consisting of [ORCLAUSE, ORCLAUSE], ORCLAUSE is a list of 
+                                        #[COURSE1, COURSE2] and COURSE is a list of [depcode, code]. COREQ is
+                                        #the same as prereq 
+        for preOrCo in prereqs: #goes into PREREQS and then COREQS
+            color +=1           
+            for orClause in preOrCo: #Each ORCLAUSE shares a color
+                color +=1
+                for singleCourse in orClause:
+                    listCourse = splitCourseString(singleCourse) #finds depcode and code, then continues the DFS
                     dep2 = listCourse[0].upper()
                     code2 = listCourse[1]
-                    buildPrereqTree(dep2, code2, tree, depth + 1)
+                    buildPrereqTree(dep2, code2, tree, depth + 1, color)
     
 class MainHandler(webapp2.RequestHandler):
     
@@ -95,24 +112,6 @@ class MainHandler(webapp2.RequestHandler):
         }
         template = JINJA_ENVIRONMENT.get_template('templates/outputPrereqs.html')
         self.response.write(template.render(template_values))
-
-    
-#    #def courseDFS(self, dep, courseInput, depth=0):
-#    #    code = courseInput.split('-')[0]
-#    #    course = getStuff(dep, code)       
-#    #    for i in course:
-#    #        #name = (i.name).encode(
-#    #        self.response.write('<font color=ffffff>'+'_' * 4 * depth + '</font>' + i.name + '<br>')
-#    #        prereqs = getCourses(i.prereqs)
-#    #        for preorco in prereqs:
-#    #            for orclause in preorco:
-#    #                  for singleCourse in orclause:
-#    #                    listCourse = splitCourseString(singleCourse)
-#    #                    dep2 = listCourse[0].upper()
-#    #                    code2 = listCourse[1]
-#    #                    self.courseDFS(dep2, code2, depth + 1)
-#    #    if depth == 0:
-#    #        self.reponse.write('<button name="reset" value ="true" type="submit">Reset</button><hr>')
                    
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
